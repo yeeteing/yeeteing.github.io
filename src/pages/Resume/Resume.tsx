68a06f17f3f0d2e6d7a1610073e4725487dccbc8
yeeteing <yeeteing96@gmail.com>
2025-09-03 13:18:37 -0400

    modify resume responsive ui

--- src/pages/Resume/Resume.tsx
+++ src/pages/Resume/Resume.tsx
@@ -9,18 +9,21 @@ interface ResumeProps {
 
 const Resume: React.FC<ResumeProps> = ({ onPageChange }) => {
   return (
-    <DisplayContainer onPageChange={onPageChange} display={{ name: "RESUME" }}>
-       <div className="resume-document-container">
-          <div className="resume-document">
-            <img
-              src="https://api.builder.io/api/v1/image/assets/TEMP/a541b0b627e35c0f47bcb3b9f89376eef0ec583a?width=1926"
-              alt="Resume Document"
-              className="resume-document-image"
-            />
+    <div className="resume-page">
+      <DisplayContainer onPageChange={onPageChange} display={{ name: "RESUME" }}>
+        <div className="resume-document-container">
+            <div className="resume-document">
+              <img
+                src="https://api.builder.io/api/v1/image/assets/TEMP/a541b0b627e35c0f47bcb3b9f89376eef0ec583a?width=1926"
+                alt="Resume Document"
+                className="resume-document-image"
+              />
+            </div>
           </div>
-        </div>
 
-    </DisplayContainer>
+      </DisplayContainer>
+    </div>
+    
   );
 };
 
