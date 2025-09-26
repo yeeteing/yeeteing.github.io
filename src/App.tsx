afdd307e66333986734b7cc7a9c79d3a81fe6c0f
yeeteing <yeeteing96@gmail.com>
2025-09-26 13:41:08 -0400

    add music background

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { NavigationBar, ProfileCard, Footer } from "./components";
 import { AboutMe, Resume, Projects, Hobbies, Contact } from "./pages";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -5,15 +6,29 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import "./global.css";
 import AudioPlayer from "./components/AudioPlayer/AudioPlayer";
+import SoundConsent from "./components/SoundConsent/SoundConsent";
 
 
 function App() {
-  
+  const [asked, setAsked] = useState(false);
+  const [play, setPlay] = useState(false);
+
+  const grant = () => {
+    setPlay(true);   // start with sound on
+    setAsked(true);
+  };
+  const deny = () => {
+    setPlay(false);  // no audio
+    setAsked(true);
+  };
   return (
     <BrowserRouter>
       <div className="app">
+        <SoundConsent open={!asked} onGrant={grant} onDeny={deny} />
         <div style={{ position: "fixed", right: 16, bottom: 16, zIndex: 1000 }}>
-        <AudioPlayer />
+        <AudioPlayer  src="https://yeeteing-portfolio-website.s3.us-east-2.amazonaws.com/other/In+Dreamland+by+Chillpeach.mp3"
+        play={play}/>
+        
       </div>
         <NavigationBar/>
         <main className="main-layout">
